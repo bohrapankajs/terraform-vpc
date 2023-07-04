68bf30cf460b73775ed5930459f4c43eea9792b9
Pankajbohra <bohrapankajs@gmail.com>
2023-07-04 16:32:49 +0530

    test

---
+++ backend.tf
@@ -0,0 +1,8 @@
+
+terraform {
+  backend "s3" {
+    bucket = "b51-tf-remote-state-buckets"
+    key    = "vpc/dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
 provider "aws" {
     region = "us-east-1"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "b51-tf-remote-state-buckets"
-    key    = "vpc/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
