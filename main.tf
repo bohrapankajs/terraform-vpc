514e5a52052357f7f8bb54e1f3024161bd5999b7
Pankajbohra <bohrapankajs@gmail.com>
2023-07-04 14:28:31 +0530

    test

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
 module "vpc" {
     source                   = "./vendor/modules/vpc"  
     VPC_CIDR                 = var.VPC_CIDR
+    PUBLIC_SUBNET_CIDR       = var.PUBLIC_SUBNET_CIDR
+    PRIVATE_SUBNET_CIDR      = var.PRIVATE_SUBNET_CIDR
+    AZ                       = var.AZ 
 }
